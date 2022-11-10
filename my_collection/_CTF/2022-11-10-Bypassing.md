4c07dc099786af62754005db256f193d6413e4dd
郑昊伦 <zhl010601@outlook.com>
2022-11-11 01:03:00 +0800

    canary to be done

---
+++ my_collection/_CTF/2022-11-10-Bypassing.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "Bypassing"
+date: 2022-11-10 20:20:00+0800
+chap: level_2
+
+---
+
+# Bypassing
+
+这一部分记录解决零字符截断和绕开`canary`保护的问题，同样是在`actf`校赛里卡了很久的题目，当时做梦都是`canary`
---
+++ my_collection/_drafts/format.c
@@ -0,0 +1,20 @@
+// format.c
+#include"format.h"
+#include<stdio.h>
+
+int main(){
+    printf("%s",g);
+    printf("%s",O);
+    printf("%s",O);
+    
+    printf("%s",D);
+
+    printf("%s",n);
+    printf("%s",I);
+    printf("%s",g);
+    printf("%s",h);
+    printf("%s",t);
+    system("pause");
+    
+    return 0;
+}
