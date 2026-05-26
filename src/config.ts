f5804d6ea94f2f87bd398c2471e6fdf327de51f0
dimshimmer <3200102570@zju.edu.cn>
2026-05-26 22:01:27 +0800

    Use dimshimmer as homepage title

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "郑昊伦",
-	subtitle: "Haolun Zheng",
+	title: "dimshimmer",
+	subtitle: "",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 205, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
